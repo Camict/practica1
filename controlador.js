603d9e37b5340aaef8f05ae3a291460c388192ca
Camict <cct40618@gmail.com>
2021-07-21 16:53:41 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,10 @@
+//programa que nos ayude a establecer
+//el costo total de una consola de video juegos
+
+//En el esterio
+//compra en dolares
+//pagar un casillero
+//(dolares costo dependiendo de la cantidad de productos)
+//envio hasta colombia
+//(cantidad de productos)
+//pagar impuestos de entrada al pais (lo digita aduanas)
